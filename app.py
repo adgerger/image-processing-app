07a8da51b22db563faedd5368c109db84dcae0a3
adgerger <adgerger@outlook.com>
2023-04-12 20:07:39 -0400

    popup generations added for each operation

--- app.py
+++ app.py
@@ -9,10 +9,82 @@ def index():
     return render_template('home.html')
 
 
+# POST requests 
 
 @app.route('/displayImageInfo', methods=['POST'])
-def run_function():
+def displayImageInfo():
     print("Post request have been invoked in app.js for displaying image information. ")
+    # Code to run the specific function goes here
+    return "Function executed successfully"
+
+
+@app.route('/cropImage', methods=['POST'])
+def cropImage():
+    print("Post request have been invoked in app.js for cropping an image. ")
+    # Code to run the specific function goes here
+    return "Function executed successfully"
+
+
+@app.route('/flipImage', methods=['POST'])
+def flipImage():
+    print("Post request have been invoked in app.js for flipping an image.")
+    # Code to run the specific function goes here
+    return "Function executed successfully"
+
+
+@app.route('/scaleImage', methods=['POST'])
+def scaleImage():
+    print("Post request have been invoked in app.js for scaling an image.")
+    # Code to run the specific function goes here
+    return "Function executed successfully"
+
+
+@app.route('/rotateImage', methods=['POST'])
+def rotateImage():
+    print("Post request have been invoked in app.js for rotating an image. ")
+    # Code to run the specific function goes here
+    return "Function executed successfully"
+
+
+@app.route('/linearMapImage', methods=['POST'])
+def linearMapImage():
+    print("Post request have been invoked in app.js for applying linear gray level mapping to image")
+    # Code to run the specific function goes here
+    return "Function executed successfully"
+
+
+@app.route('/powerLawMapImage', methods=['POST'])
+def powerLawMapImage():
+    print("Post request have been invoked in app.js for power law mapping an image. ")
+    # Code to run the specific function goes here
+    return "Function executed successfully"
+
+
+@app.route('/calculateHistogram', methods=['POST'])
+def calculateHistogram():
+    print("Post request have been invoked in app.js for calculating the histogram of an image. ")
+    # Code to run the specific function goes here
+    return "Function executed successfully"
+
+
+@app.route('/histogramEqualizeImage', methods=['POST'])
+def equalizeHistogram():
+    print("Post request have been invoked in app.js for histogram equalizing an image. ")
+    # Code to run the specific function goes here
+    return "Function executed successfully"
+
+
+@app.route('/convoluteImage', methods=['POST'])
+def convoluteImage():
+    print("Post request have been invoked in app.js for applying convolution to an image. ")
+    # Code to run the specific function goes here
+    return "Function executed successfully"
+
+
+
+@app.route('/filterImage', methods=['POST'])
+def filterImage():
+    print("Post request have been invoked in app.js for applying non-linear filterin to an image. ")
     # Code to run the specific function goes here
     return "Function executed successfully"
 
--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,205 @@
 
+window.onload = function() {
 
-function displayInfoHandler() {
-    console.log("displayInfoHandler invoked on javascript.");
-    fetch('/displayImageInfo', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=UTF-8'
+    /* Get all the popups, operation buttons and close buttons in the document. */
+    const allPopups = document.querySelectorAll('.popup-container');
+    const allButtons = document.querySelectorAll(".operation-button");
+    const closeButtons = document.querySelectorAll('.close-button');
+    
+    /* Make it so that every operation button opens up it's respective popup when clicked. */
+    allButtons.forEach(function(element, index) {
+        element.addEventListener('click', () => {
+            allPopups[index].style.display = 'flex';
+        });
+    });
+
+    /* Add closing functionality that makes sure every popup is closed when clicked, for each close button. */
+    closeButtons.forEach(button => {
+        button.addEventListener('click', () => { 
+            allPopups.forEach(popup => {
+            popup.style.display = 'none';
+            })
+        });
+    });
+
+    
+    /* Functions for all POST requests to the backend */
+    
+    function displayInfoHandler() {
+        console.log("displayInfoHandler invoked on javascript.");
+    
+        fetch('/displayImageInfo', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json;charset=UTF-8'
+          }
+        })
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.error(error));
       }
-    })
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.error(error));
-  }
+    
+    
+    function cropHandler() {
+        console.log("cropHandler invoked on javascript.");
+    
+        fetch('/cropImage', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json;charset=UTF-8'
+            }
+          })
+          .then(response => response.text())
+          .then(result => console.log(result))
+          .catch(error => console.error(error));
+    
+      }
+    
+      
+    function flipHandler() {
+        console.log("flipHandler invoked on javascript.");
+    
+        fetch('/flipImage', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json;charset=UTF-8'
+            }
+          })
+          .then(response => response.text())
+          .then(result => console.log(result))
+          .catch(error => console.error(error));
+    
+      }
+    
+    
+    function scaleHandler() {
+        console.log("displayInfoHandler invoked on javascript.");
+    
+        fetch('/scaleImage', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json;charset=UTF-8'
+          }
+        })
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.error(error));
+      }
+      
+    
+    function rotateHandler() {
+        console.log("rotateHandler invoked on javascript.");
+    
+        fetch('/rotateImage', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json;charset=UTF-8'
+            }
+          })
+          .then(response => response.text())
+          .then(result => console.log(result))
+          .catch(error => console.error(error));
+    
+      }
+    
+    
+    function linearMapHandler() {
+        console.log("linearMapHandler invoked on javascript.");
+    
+        fetch('/linearMapImage', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json;charset=UTF-8'
+            }
+          })
+          .then(response => response.text())
+          .then(result => console.log(result))
+          .catch(error => console.error(error));
+    
+      }
+    
+    
+    function powerLawMapHandler() {
+        console.log("powerLawMapHandler invoked on javascript.");
+    
+        fetch('/powerLawMapImage', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json;charset=UTF-8'
+            }
+          })
+          .then(response => response.text())
+          .then(result => console.log(result))
+          .catch(error => console.error(error));
+    
+      }
+    
+    
+    function histogramHandler() {
+        console.log("histogramHandler invoked on javascript.");
+    
+        fetch('/calculateHistogram', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json;charset=UTF-8'
+            }
+          })
+          .then(response => response.text())
+          .then(result => console.log(result))
+          .catch(error => console.error(error));
+    
+      }
+    
+    
+    function equalizeHistogramHandler() {
+        console.log("equalizeHistogramHandler invoked on javascript.");
+    
+        fetch('/histogramEqualizeImage', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json;charset=UTF-8'
+            }
+          })
+          .then(response => response.text())
+          .then(result => console.log(result))
+          .catch(error => console.error(error));
+    
+      }
+    
+    
+    function convolutionHandler() {
+        console.log("convolutionHandler invoked on javascript.");
+    
+        fetch('/convoluteImage', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json;charset=UTF-8'
+            }
+          })
+          .then(response => response.text())
+          .then(result => console.log(result))
+          .catch(error => console.error(error));
+    
+      }
+    
+    
+    function nonLinearFilterHandler() {
+        console.log("nonLinearFilterHandler invoked on javascript.");
+    
+        fetch('/filterImage', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json;charset=UTF-8'
+            }
+          })
+          .then(response => response.text())
+          .then(result => console.log(result))
+          .catch(error => console.error(error));
+    
+      }
+
+};
+
 
 
 
